1ca815aaa126190e249b74f358f63fb3900a3d00
Jace Lee <jarre9519@gmail.com>
2021-04-29 14:07:33 +0900

    Complete MONGODB setup and connection

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+
+dotenv.config()
 
 const app = express()
 
-const PORT = 5000
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+mongoose.connection
+  .once('open', () => {
+    console.log('MONGODB is connected')
+  })
+  .on('error', err => {
+    console.log(`MONGODB connection error: ${err}`)
+  })
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`)
