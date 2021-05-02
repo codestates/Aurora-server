f3c27f7d4e30fbe877f8361eece765c9c65644ae
ha-jinwoo <gkwlsdn95@naver.com>
2021-05-02 14:54:06 +0900

    Feat access token

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { signup, activateEmail, signin, deliverAccessToken, deliverUserInfo, signout } = require('../controllers/auth')
+const { signup, activateEmail, signin, deliverAccessToken, deliverUserInfo, signout, deleteUserInfo } = require('../controllers/auth')
 const { getGoogleAuthControl, deliverOauthInfo, deliverOauthToken } = require('../controllers/oauth')
 
 router.post('/signup', signup)
 router.post('/signin', signin)
 router.post('/refresh_token', deliverAccessToken)
 router.post('/activation', activateEmail)
-router.get('/deliverUserInfo', deliverUserInfo)
+router.post('/delete_user_info', deleteUserInfo)
+router.get('/deliver_user_info', deliverUserInfo)
 router.get('/signout', signout)
 // Oauth
 router.get('/auth/google/url', getGoogleAuthControl)
