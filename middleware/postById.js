9605345b502e480e926ad8f8320668a1a04e1d77
Jace Lee <jarre9519@gmail.com>
2021-05-03 20:57:09 +0900

    Add middlewares
    1. auth middleware
    2. postById middleware
    3. poster middleware

---
+++ middleware/postById.js
@@ -0,0 +1,26 @@
+const Post = require('../models/post')
+
+const postById = (req, res, next, postId) => {
+  try {
+    Post.findById(postId)
+      .populate('postedBy', '_id, name')
+      .populate('comments.postedBy', '_id name')
+      .select()
+      .lean()
+      .exec((err, post) => {
+        if (err) {
+          return res.status(400).json({
+            message: '게시물이 존재하지 않습니다'
+          })
+        }
+
+        req.post = post
+
+        next()
+      })
+  } catch (error) {
+    return res.status(500).send({ message: '서버 에러로 요청을 처리할 수 없습니다' })
+  }
+}
+
+module.exports = postById
