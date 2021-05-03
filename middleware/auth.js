b5e4ede35a6a2708170c7a8e1ed8aad8e75c137a
Jace Lee <jarre9519@gmail.com>
2021-05-03 20:57:09 +0900

    Add middlewares
    1. auth middleware
    2. postById middleware
    3. poster middleware

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const { verifyAccessToken } = require('../helpers/tokens')
+
+const auth = async (req, res, next) => {
+  try {
+    const accessToken = req.header('Authorization')
+
+    if (!accessToken) {
+      return res.status(400).json({ message: '다시 로그인을 해주세요' })
+    }
+
+    const user = await verifyAccessToken(accessToken)
+
+    req.userId = user
+
+    next()
+  } catch (error) {
+    return res.status(500).send({ message: '서버 에러로 요청을 처리할 수 없습니다' })
+  }
+}
+
+module.exports = auth
