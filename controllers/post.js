4be990b21aa63dc45ee32116af5d0faef433911f
Jace Lee <jarre9519@gmail.com>
2021-05-03 23:30:11 +0900

    Add post controller logics
    Add createPost logic
    
    Add getPosts logic
    
    Add updatePost logic
    
    Add deletePost logic
    
    Add try-catch block to post controller

--- controllers/post.js
+++ controllers/post.js
@@ -3,65 +3,124 @@ const { IncomingForm } = require('formidable')
 const fs = require('fs')
 
 exports.createPost = (req, res, next) => {
-  // maxFieldsSize === default 20mb
-  const formOptions = {
-    multiples: true,
-    keepExtensions: true
-  }
-  const form = new IncomingForm(formOptions)
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return res.status(400).json({
-        error: `Post upload error, ${err}`
-      })
+  try {
+    const formOptions = {
+      multiples: true,
+      keepExtensions: true
     }
-    const imageFiles = files['images[]']
+    const form = new IncomingForm(formOptions)
 
-    if (!Array.isArray(imageFiles) && imageFiles.name === '') {
-      return res.status(400).json({
-        message: 'Please put images in your post'
-      })
-    }
-
-    const newPost = new Post(fields)
-
-    if (Array.isArray(imageFiles)) {
-      imageFiles.forEach(image => {
-        newPost.images.push({
-          data: fs.readFileSync(image.path),
-          contentType: image.type
-        })
-      })
-    } else {
-      newPost.images.push({
-        data: fs.readFileSync(imageFiles.path),
-        contentType: imageFiles.type
-      })
-    }
-
-    newPost.save((err, result) => {
-      if (err) {
-        return res.status(400).json({ err })
-      }
-      res.status(201).json(result)
-    })
-  })
-}
-
-exports.getPostsByUser = (req, res) => {
-  // test user id: 608d062ea5b357985c1f7aa8
-  Post.find({ postedBy: req.user._id })
-    .populate('postedBy', '-_id username')
-    .sort({ createdAt: -1 })
-    .lean()
-    .exec((err, posts) => {
+    form.parse(req, (err, fields, files) => {
       if (err) {
         return res.status(400).json({
-          error: 'Bad request'
+          message: '게시물을 올릴 수 없습니다'
+        })
+      }
+      const imageFiles = files['images[]']
+
+      if (!Array.isArray(imageFiles) && imageFiles.name === '') {
+        return res.status(400).json({
+          message: '게시물에 이미지를 넣어주세요'
         })
       }
 
-      res.status(200).json(posts)
+      const newPost = new Post(fields)
+
+      if (Array.isArray(imageFiles)) {
+        imageFiles.forEach(image => {
+          newPost.images.push({
+            data: fs.readFileSync(image.path),
+            contentType: image.type
+          })
+        })
+      } else {
+        newPost.images.push({
+          data: fs.readFileSync(imageFiles.path),
+          contentType: imageFiles.type
+        })
+      }
+
+      newPost.postedBy = req.user.id
+
+      newPost.save((err, post) => {
+        if (err) {
+          return res.status(400).json({ message: '게시물을 저장하던 중, 에러가 발생했습니다' })
+        }
+        res.status(201).json(post)
+      })
     })
+  } catch (error) {
+    return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' })
+  }
+}
+
+exports.getPosts = async (req, res) => {
+  try {
+    const page = req.query.page
+    const postsPerPage = 5
+    const totalPosts = await Post.find({ postedBy: req.user.id }).countDocuments()
+
+    Post.find({ postedBy: req.user.id })
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * postsPerPage)
+      .limit(postsPerPage)
+      .populate('postedBy', '_id username')
+      .lean()
+      .exec((err, posts) => {
+        if (err) {
+          return res.status(400).json({ message: '게시물들을 찾을 수 없습니다' })
+        }
+        res.status(201).json({
+          posts,
+          totalPosts
+        })
+      })
+  } catch (error) {
+    return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' })
+  }
+}
+
+exports.updatePost = (req, res) => {
+  try {
+    const formOptions = {
+      multiples: true,
+      keepExtensions: true
+    }
+    const form = new IncomingForm(formOptions)
+    const editingPost = req.post
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return res.status(400).json({
+          message: '게시물을 수정할 수 없습니다'
+        })
+      }
+
+      editingPost.set({ content: fields.content })
+
+      editingPost.save((err, post) => {
+        if (err) {
+          return res.status(400).json({ message: '게시물을 수정하던 중, 에러가 발생했습니다' })
+        }
+        res.status(200).json(post)
+      })
+    })
+  } catch (error) {
+    return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' })
+  }
+}
+
+exports.deletePost = (req, res) => {
+  const post = req.post
+
+  try {
+    post.remove((err, post) => {
+      if (err) {
+        return res.status(400).json({ message: '게시물을 삭제하던 중, 에러가 발생했습니다' })
+      }
+      res.status(200).json({ message: '게시물이 정상적으로 삭제되었습니다' })
+    })
+  } catch (error) {
+    return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' })
+  }
 }
--- controllers/tokenFunctions.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-exports.isAuthorized = (req) => {
-  const token = req.headers.Authorization
-  if (!token) return null
-  return jwt.verify(token, process.env.ACCESS_SECRET_KEY)
-}
