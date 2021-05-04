5c38429e2463512b874a29fb9e14bfa238d28d5c
Jace Lee <jarre9519@gmail.com>
2021-05-04 23:01:06 +0900

    Add updateComment logic

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,4 +1,5 @@
 const Comment = require('../models/comment')
+const Post = require('../models/post')
 const User = require('../models/user')
 
 exports.createComment = async (req, res) => {
@@ -10,20 +11,42 @@ exports.createComment = async (req, res) => {
     }
 
     const user = await User.findById(req.user.id).select('_id username')
-
     const post = req.post
     const newComment = new Comment({
       content,
       commentedBy: user
     })
 
-    post.comments.push(newComment)
+    newComment.save()
 
+    post.comments.push(newComment)
     post.save((err, post) => {
       if (err) {
-        return res.status(400).json({ message: '댓글을 저장하던 중, 에러가 발생했습니다' })
+        return res.status(400).json({ message: '댓글을 작성하던 중, 에러가 발생했습니다' })
       }
       return res.status(200).json(newComment)
+    })
+  } catch (error) {
+    return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' })
+  }
+}
+
+exports.updateComment = async (req, res) => {
+  try {
+    const { content } = req.body
+
+    if (!content.length) {
+      return res.status(400).json({ message: '댓글을 수정할 수 없습니다' })
+    }
+
+    const comment = req.comment
+
+    comment.set({ content })
+    comment.save((err, comment) => {
+      if (err) {
+        return res.status(400).json({ message: '댓글을 수정하던 중, 에러가 발생했습니다' })
+      }
+      return res.status(200).json(comment)
     })
   } catch (error) {
     return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' })
--- routes/comment.js
+++ routes/comment.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const auth = require('../middleware/auth')
 const postById = require('../middleware/postById')
-const { createComment } = require('../controllers/comment')
+const commentById = require('../middleware/commentById')
+const commenter = require('../middleware/commenter')
+const { createComment, updateComment } = require('../controllers/comment')
 
 const router = express.Router()
 
 router.post('/post/:postId/comment', auth, createComment)
+router.patch('/post/:postId/comment/:commentId', auth, commenter, updateComment)
 
 router.param('postId', postById)
+router.param('commentId', commentById)
 
 module.exports = router
