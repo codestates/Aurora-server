e705f2f4b50712efb2d01cd832efbbeda2120b11
Jace Lee <jarre9519@gmail.com>
2021-05-06 11:27:10 +0900

    Refactor: likes

--- routes/like.js
+++ routes/like.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const auth = require('../middleware/auth')
-const { getAllLikes, getLikes, deleteLikes } = require('../controllers/like')
+const { getAllLikes, addLikes, deleteLikes } = require('../controllers/like')
 
 const router = express.Router()
 
 router.get('/like', auth, getAllLikes)
-router.post('/like', auth, getLikes)
+router.post('/like', auth, addLikes)
 router.delete('/like', auth, deleteLikes)
 
 module.exports = router
